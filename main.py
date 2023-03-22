8ba08de916166627a8142fe34b121bf7902be98f
yota priyanto <yota.priyanto@gmail.com>
2023-03-22 22:22:26 +0700

    emergency

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-##test case here
+#insert routing class
+@app.route('/api/tweets')
+def twit_data():
+    #do something here
+    return
+
+if __name__ == '__main__':
+    app.run(debug=True)
